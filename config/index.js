8a59fd4eb98de618bf7974a67d80dc826213d9d4
santai-wang <112493856+santai-wang@users.noreply.github.com>
2022-08-30 22:23:57 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -10,7 +10,7 @@ export const config = {
     // 模板消息id
     TEMPLATE_ID: "9BhW1XnOwGo_xhsvMql77P4f6IrJ1MHeW4L_gCrm0EI",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    USERS: ["oBGs-5j7sb5UU8hTe_GlWViu_CLA"],
+    USERS: ["oBGs-5j7sb5UU8hTe_GlWViu_CLA","oBGs-5vYCHGX4BVXG8bSntSBkY5s"],
      
     /**
      * 信息配置
