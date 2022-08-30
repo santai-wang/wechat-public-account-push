41c36d798a170167773dea8bcd52bf7e14c84e14
santai-wang <112493856+santai-wang@users.noreply.github.com>
2022-08-30 22:46:53 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ export const config = {
     // 公众号APP_SECRET
     APP_SECRET: "46be94ada8eeb94e39e72c1105637702",
     // 模板消息id
-    TEMPLATE_ID: "9BhW1XnOwGo_xhsvMql77P4f6IrJ1MHeW4L_gCrm0EI",
+    TEMPLATE_ID: "i_1nVhrfXkcMoJ1YFIUtWfS25UC7kdr4m5U3ZaJZ14k",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
     USERS: ["oBGs-5j7sb5UU8hTe_GlWViu_CLA","oBGs-5vYCHGX4BVXG8bSntSBkY5s"],
      
@@ -30,7 +30,7 @@ export const config = {
     // 生日时，name填写想要展示的名字，你可以填“美丽可爱亲亲老婆”
     // 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
     FESTIVALS: [
-      {"type": "生日", "name": "小宝", "year": "1997", "date": "01-02"},
+      {"type": "生日", "name": "小丁同学", "year": "1997", "date": "01-02"},
     ],
     // 限制重要节日的展示条目, 需要填写数字; 
     // 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
@@ -41,15 +41,15 @@ export const config = {
 
     // 在一起的日子，格式同上
     LOVE_DATE: "2018-07-29",
-    // 结婚纪念日
-    MARRY_DATE: "2020-01-04",
+    // 喜欢你的
+    MARRY_DATE: "2018-07-01",
 
 
     /** 每日一言 */
 
     // 好文节选的内容类型
     // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
-    LITERARY_PREFERENCE: "原创"
+    LITERARY_PREFERENCE: "其他"
 
 
     }
